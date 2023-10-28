6c724eda0cac8f5f89505d894c0880f947b7fb45
wahid80382 <134073438+wahid80382@users.noreply.github.com>
2023-10-28 20:19:39 +0600

    fgnfgnfgnhgn
    nnfgngn

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,12 +1,14 @@
 $(function(){
-    //Toggle Search Modal
+    //Toggle Search Modal js start
     $('#search_btn').on('click', function(){
         $('#search_modal').addClass('show');
     })
     $('#search_close_btn').on('click', function(){
         $('#search_modal').removeClass('show');
     })
+    //Toggle Search Modal js end
 
+    //banner_slider js start
     $('.banner_slider').slick({
         autoplay: true,
         autoplaySpeed:1500,
@@ -14,8 +16,33 @@ $(function(){
         dotsClass:`slider_dot`,
         arrows:false,
       });
-    
+      //banner_slider js end
 
-    
-          
+      //new_arrivals_products_slider js start
+      $('.product_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+      });
+      //new_arrivals_products_slider js end
+
+      $('.Deals_Of_The_Days_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots:true,
+        dotsClass:`slider_dot`,
+        arrows:false,
+      }); 
+      //new_arrivals_products_slider js start
+
+      $('.Latest_News_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
 })
